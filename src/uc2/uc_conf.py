56bb98dc5be69521db5496c18d776c7c93d908db
igor.e.novikov <Igor.E.Novikov@gmail.com>
2011-04-06 09:36:49 +0000

    object fill and stroke

--- src/uc2/uc_conf.py
+++ src/uc2/uc_conf.py
@@ -16,6 +16,7 @@
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import math
 
 from uc2.utils.fs import expanduser_unicode
 
@@ -27,7 +28,7 @@ def dummy_translate(str):
 
 in_to_pt = 72.0
 cm_to_pt = in_to_pt / 2.54
-mm_to_pt = cm_to_pt / 10
+mm_to_pt = cm_to_pt / 10 #2,8346456693
 m_to_pt	 = 100 * cm_to_pt
 
 pt_to_in = 1.0 / 72.0
@@ -39,7 +40,7 @@ pt_to_m	 = pt_to_cm / 100
 unit_dict = {'pt': 1.0, 'in': in_to_pt, 'cm': cm_to_pt, 'mm': mm_to_pt}
 unit_names = ['pt', 'in', 'cm', 'mm']
 
-PAGE_FORMATS = {  
+PAGE_FORMATS = {
 			'A0': (2383.9370078740158, 3370.3937007874015),
 			'A1': (1683.7795275590549, 2383.9370078740158),
 			'A2': (1190.5511811023621, 1683.7795275590549),
@@ -64,41 +65,128 @@ PAGE_FORMATS = {
 			'Half Letter': (396.0, 612.0),
 			'Visit card #1': (141.73228346456693, 255.11811023622045),
 			'Visit card #2': (155.90551181102362, 240.94488188976379),
-			} 
+			}
 
 PORTRAIT = 0
-LANDSCAPE = 1 
+LANDSCAPE = 1
+
+#Fill and stroke constants
+
+COLOR_GRAY = 'GRAY'
+COLOR_RGB = 'RGB'
+COLOR_CMYK = 'CMYK'
+COLOR_LAB = 'LAB'
+COLOR_SPOT = 'SPOT'
+
+FILL_EVENODD = 1
+FILL_NONZERO = 0
+
+FILL_SOLID = 0
+FILL_GRADIENT = 1
+FILL_TILED = 2
+
+STROKE_MIDDLE = 0
+STROKE_OTSIDE = 1
+STROKE_INSIDE = 2
+
+JOIN_MITER = 0
+JOIN_ROUND = 1
+JOIN_BEVEL = 2
+
+
+DEFAULT_MITER_ANGLE = 45.0
+DEFAULT_MITER_LIMIT = 1 / math.sin(DEFAULT_MITER_ANGLE / 2.0)
+
+CAP_BUTT = 1
+CAP_ROUND = 2
+CAP_SQUARE = 3
+
 
 class UCData:
-		
+
 	app_name = 'UniConvertor'
 	app_icon = None
-	
+
 	app_config_dir = expanduser_unicode(os.path.join('~', '.config', 'uc2'))
 	if not os.path.lexists(app_config_dir):
 		os.makedirs(app_config_dir)
 	app_config = expanduser_unicode(os.path.join('~', '.config', 'uc2', 'preferences.cfg'))
-	
-	
+
+
 class UCConfig(XmlConfigParser):
-	
+
 	#============== GENERIC SECTION ===================
 	uc_version = ''
 	system_encoding = 'utf-8'	# default encoding for sK1 (GUI uses utf-8 only)
-	
+
 	#============== DOCUMENT SECTION ==================
-	
+
 	# 0 - page center
 	# 1 - lower-left page corner
 	# 2 - upper-left page corner 
 	doc_origin = 1
-	
+
 	page_format = 'A4'
 	page_orientation = PORTRAIT
-	
+
 	layer_color = '#3252A2'
 	guide_color = '#0051FF'
 	grid_color = '#D3DEE8'
 	master_layer_color = '#000000'
-	
+
 	grid_geometry = [0, 0, 2.83465, 2.83465]
+
+#Fill description:
+#   [] empty fill
+
+#	[fill rule,
+#	 fill type,
+#	 color]
+
+#	[fill rule,
+#	 FILL_GRADIENT,
+#	 gradient]
+
+#	[fill rule,
+#	 FILL_TILED,
+#	 pattern]
+
+	default_fill = []
+
+#Stroke description:
+#   [] - empty stroke
+
+#	[stroke rule,
+#	 stroke width,
+#	 color,
+#	 dash,
+#	 caps,
+#	 joint,
+#	 miter limit, - miter limit = 1/sin(angle/2)
+#	 behind flag,
+#	 scalable flag,
+#	 markers,
+#	]
+
+	default_stroke = [
+					STROKE_MIDDLE,
+					0.1 * mm_to_pt,
+					[COLOR_CMYK, [0.0, 0.0, 0.0, 1.0], 1.0, 'Black'],
+					[],
+					CAP_BUTT,
+					JOIN_MITER,
+					DEFAULT_MITER_LIMIT,
+					0,
+					0,
+					[],
+					]
+
+#Text description:
+#   [] - no style
+
+	default_text = []
+
+#Structural style will contain opacity, filters etc.
+#Now it's just reserved
+
+	default_structural_style = []
