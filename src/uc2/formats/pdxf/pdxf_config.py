fa9087941aeb91d94f5e799f4320aa792a29c1ef
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-04-24 06:32:43 +0000

    minor changes

--- src/uc2/formats/pdxf/pdxf_config.py
+++ src/uc2/formats/pdxf/pdxf_config.py
@@ -77,7 +77,7 @@ class PDXF_Config(XmlConfigParser):
 	default_text_style = []
 	default_structural_style = []
 
-	#============== COLORMANAGEMENT SECTION ===================
+	#============== COLOR MANAGEMENT SECTION ===================
 	default_rgb_profile = ''
 	default_cmyk_profile = ''
 	default_lab_profile = ''
