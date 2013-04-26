ca3637a7367bd6b6d3194fcda1fbef1614dee148
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-04-26 13:21:08 +0000

    constants for structural elements

--- src/uc2/formats/sk1/sk1const.py
+++ src/uc2/formats/sk1/sk1const.py
@@ -14,3 +14,19 @@
 #	
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+RGB = 'RGB'
+CMYK = 'CMYK'
+SPOT = 'SPOT'
+
+default_color = [RGB, [0.0, 0.0, 0.0], 1.0, '']
+default_sk1color = (RGB, 0.0, 0.0, 0.0)
+
+default_grid = (0, 0, 2.83465, 2.83465)
+default_grid_color = [RGB, [0.83, 0.87, 0.91], 1.0, '']
+
+default_layer_properties = [1, 1, 0, 0]
+default_layer_color = [RGB, [0.196, 0.314, 0.635], 1.0, '']
+
+default_guidelayer_properties = [1, 0, 0, 1]
+default_guidelayer_color = [RGB, [0.0, 0.3, 1.0], 1.0, '']
