1285ffc2577ad324bbe2da57ab27136016ba0c92
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@f4ca8763-45e9-a9ea-a118-b73bb44545da>
2013-04-26 14:02:24 +0000

    default style

--- trunk/src/uc2/formats/sk1/sk1const.py
+++ trunk/src/uc2/formats/sk1/sk1const.py
@@ -15,12 +15,17 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-RGB = 'RGB'
-CMYK = 'CMYK'
-SPOT = 'SPOT'
+import math
 
-default_color = [RGB, [0.0, 0.0, 0.0], 1.0, '']
-default_sk1color = (RGB, 0.0, 0.0, 0.0)
+from uc2 import uc2const
+from uc2.formats.pdxf import const
+
+RGB = uc2const.COLOR_RGB
+CMYK = uc2const.COLOR_CMYK
+SPOT = uc2const.COLOR_SPOT
+
+fallback_color = [RGB, [0.0, 0.0, 0.0], 1.0, '']
+fallback_sk1color = (RGB, 0.0, 0.0, 0.0)
 
 default_grid = (0, 0, 2.83465, 2.83465)
 default_grid_color = [RGB, [0.83, 0.87, 0.91], 1.0, '']
@@ -30,3 +35,33 @@ default_layer_color = [RGB, [0.196, 0.314, 0.635], 1.0, '']
 
 default_guidelayer_properties = [1, 0, 0, 1]
 default_guidelayer_color = [RGB, [0.0, 0.3, 1.0], 1.0, '']
+
+
+fill_style = []
+default_stroke_rule = const.STROKE_MIDDLE
+default_stroke_width = 0.0
+default_stroke_color = [uc2const.COLOR_CMYK, [0.0, 0.0, 0.0, 1.0], 1.0, 'Black']
+default_stroke_dash = []
+default_stroke_cap = const.CAP_BUTT
+default_stroke_join = const.JOIN_MITER
+default_stroke_miter_angle = 45.0
+default_stroke_miter_limit = 1.0 / math.sin(default_stroke_miter_angle / 2.0)
+default_stroke_behind_flag = 0
+default_stroke_scalable_flag = 0
+default_stroke_markers = []
+
+stroke_style = [
+				default_stroke_rule,
+				default_stroke_width,
+				default_stroke_color,
+				default_stroke_dash,
+				default_stroke_cap,
+				default_stroke_join,
+				default_stroke_miter_limit,
+				default_stroke_behind_flag,
+				default_stroke_scalable_flag,
+				default_stroke_markers,
+				]
+text_style = []
+structural_style = []
+default_style = [fill_style, stroke_style, text_style, structural_style]
