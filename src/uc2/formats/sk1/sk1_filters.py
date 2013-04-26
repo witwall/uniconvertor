d5fb70272ce59cd4367689008966e1106f973973
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-04-26 13:22:17 +0000

    data processing while parsing

--- src/uc2/formats/sk1/sk1_filters.py
+++ src/uc2/formats/sk1/sk1_filters.py
@@ -17,7 +17,8 @@
 
 import sys, os
 
-from uc2 import _, events, msgconst
+from uc2 import _, events, msgconst, uc2const
+from uc2.formats.pdxf import const
 from uc2.formats.sk1.model import SK1Document, SK1Layout, SK1Grid, SK1Page, \
 SK1Layer, SK1MasterLayer, SK1GuideLayer, SK1Guide, SK1Group, SK1MaskGroup, \
 SK1Rectangle, SK1Ellipse, SK1Curve, SK1Text, SK1BitmapData, SK1Image
@@ -26,7 +27,7 @@ class SK1_Loader:
 	name = 'SK1_Loader'
 	presenter = None
 	config = None
-	path = None
+	paths = []
 	options = {}
 	model = None
 
@@ -125,19 +126,35 @@ class SK1_Loader:
 		self.add_object(SK1Document(self.config))
 
 	def layout(self, *args):
-		self.add_object(SK1Layout(self.config), self.model)
+		if len(args) > 2:
+			format = args[0]
+			size = args[1]
+			orientation = args[2]
+		else:
+			if isinstance(args[0], str):
+				format = args[0]
+				orientation = args[1]
+				if not format in uc2const.PAGE_FORMAT_NAMES: format = 'A4'
+				size = uc2const.PAGE_FORMATS[format]
+			else:
+				format = ''
+				size = args[0]
+				orientation = args[1]
+		obj = SK1Layout(self.config, format, size, orientation)
+		self.add_object(obj, self.model)
 
-	def grid(self, *args):
-		self.add_object(SK1Grid(self.config), self.model)
+	def grid(self, grid, visibility, grid_color, layer_name):
+		obj = SK1Grid(self.config, grid, visibility, grid_color, layer_name)
+		self.add_object(obj, self.model)
 
-	def page(self, *args):
-		page = SK1Page(self.config)
+	def page(self, name, format, size, orientation):
+		page = SK1Page(self.config, name, format, size, orientation)
 		self.active_page = page
 		self.active_layer = None
 		self.parent_stack = []
 		self.add_object(page, self.model)
 
-	def layer(self, *args):
+	def layer(self, name, p1, p2, p3, p4, layer_color):
 		if self.active_page is None:
 			page = SK1Page(self.config)
 			current_line = self.line
@@ -148,41 +165,44 @@ class SK1_Loader:
 			self.parent_stack = []
 			self.add_object(page, self.model)
 			self.line = current_line
-		layer = SK1Layer(self.config)
+		properties = [p1, p2, p3, p4]
+		layer = SK1Layer(self.config, name, properties, layer_color)
 		self.active_layer = layer
 		self.add_object(layer, self.active_page)
 
-	def masterlayer(self, *args):
-		mlayer = SK1MasterLayer(self.config)
+	def masterlayer(self, name, p1, p2, p3, p4, layer_color):
+		properties = [p1, p2, p3, p4]
+		mlayer = SK1MasterLayer(self.config, name, properties, layer_color)
 		self.active_layer = mlayer
 		self.add_object(mlayer, self.model)
 
-	def guidelayer(self, *args):
-		glayer = SK1GuideLayer(self.config)
+	def guidelayer(self, name, p1, p2, p3, p4, layer_color):
+		properties = [p1, p2, p3, p4]
+		glayer = SK1GuideLayer(self.config, name, properties, layer_color)
 		self.active_layer = glayer
 		self.add_object(glayer, self.model)
 
-	def guide(self, *args):
+	def guide(self, point, orientation):
 		self.add_object(SK1Guide(self.config))
 
 	#---GROUPS
-	def G(self, *args):
+	def G(self):
 		group = SK1Group(self.config)
 		self.add_object(group)
 		self.parent_stack.append(group)
 
-	def G_(self, *args):
+	def G_(self):
 		self.parent_stack = self.parent_stack[:-1]
 
-	def M(self, *args):
+	def M(self):
 		mgroup = SK1MaskGroup(self.config)
 		self.add_object(mgroup)
 		self.parent_stack.append(mgroup)
 
-	def M_(self, *args):
+	def M_(self):
 		self.parent_stack = self.parent_stack[:-1]
 
-	def B(self, *args):
+	def B(self):
 		group = SK1Group(self.config)
 		self.string = group.string
 		self.line = ''
@@ -191,10 +211,10 @@ class SK1_Loader:
 
 	def Bi(self, *args):self.string = ''
 
-	def B_(self, *args):
+	def B_(self):
 		self.parent_stack = self.parent_stack[:-1]
 
-	def PT(self, *args):
+	def PT(self):
 		group = SK1Group(self.config)
 		self.string = group.string
 		self.line = ''
@@ -203,7 +223,7 @@ class SK1_Loader:
 
 	def pt(self, *args):self.string = ''
 
-	def PT_(self, *args):
+	def PT_(self):
 		self.parent_stack = self.parent_stack[:-1]
 
 	def PC(self, *args):
@@ -213,33 +233,59 @@ class SK1_Loader:
 		self.add_object(group)
 		self.parent_stack.append(group)
 
-	def PC_(self, *args):
+	def PC_(self):
 		self.parent_stack = self.parent_stack[:-1]
 
 	#---PRIMITIVES
-	def r(self, *args):
-		self.add_object(SK1Rectangle(self.config))
+	def r(self, m11, m12, m21, m22, dx, dy, radius1=None, radius2=None):
+		trafo = (m11, m12, m21, m22, dx, dy)
+		self.add_object(SK1Rectangle(self.config, trafo, radius1, radius2))
 
-	def e(self, *args):
-		self.add_object(SK1Ellipse(self.config))
+	def e(self, m11, m12, m21, m22, dx, dy, start_angle=None, end_angle=None, arc_type=None):
+		trafo = (m11, m12, m21, m22, dx, dy)
+		self.add_object(SK1Ellipse(self.config, trafo, start_angle, end_angle, arc_type))
 
-	def b(self, *args): self.add_string(self.line)
-	def bs(self, *args): self.add_string(self.line)
-	def bc(self, *args): self.add_string(self.line)
-	def bn(self, *args): self.add_string(self.line)
-	def bC(self, *args):
-		self.add_object(SK1Curve(self.config))
+	def b(self):
+		self.paths = [[None, [], const.CURVE_OPENED]]
+		self.add_object(SK1Curve(self.config, self.paths))
 
-	def txt(self, *args):
-		self.add_object(SK1Text(self.config))
+	def bs(self, x, y, cont):
+		point = [x, y]
+		path = self.paths[-1]
+		points = path[1]
+		if path[0] is None:
+			path[0] = point
+		else:
+			points.append(point)
 
-	def bm(self, *args):
-		bmd_obj = SK1BitmapData(self.config)
+	def bc(self, x1, y1, x2, y2, x3, y3, cont):
+		point = [[x1, y1], [x2, y2], [x3, y3], cont]
+		path = self.paths[-1]
+		points = path[1]
+		if path[0] is None:
+			path[0] = point
+		else:
+			points.append(point)
+
+	def bn(self):
+		self.paths.append([None, [], const.CURVE_OPENED])
+
+	def bC(self):
+		self.paths[-1][2] = const.CURVE_CLOSED
+
+	def txt(self, text, trafo, horiz_align, vert_align, chargap, wordgap, linegap):
+		self.add_object(SK1Text(self.config, text, trafo, horiz_align, vert_align, chargap, wordgap, linegap))
+
+	def bm(self, id):
+		bmd_obj = SK1BitmapData(self.config, id)
 		self.add_object(bmd_obj)
-		bmd_obj.read_data(self.file)
+		try:
+			bmd_obj.read_data(self.file)
+		except:pass
+		self.presenter.resources[id] = bmd_obj.raw_image
 
-	def im(self, *args):
-		self.add_object(SK1Image(self.config))
+	def im(self, trafo, id):
+		self.add_object(SK1Image(self.config, trafo, id))
 
 	def eps(self, *args):self.string = ''
 
@@ -261,5 +307,6 @@ class SK1_Saver:
 			events.emit(events.MESSAGES, msgconst.ERROR, msg)
 			raise IOError(errtype, msg + '\n' + value, traceback)
 
+		presenter.update()
 		presenter.model.write_content(file)
 		file.close()
