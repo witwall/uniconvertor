09f6d56bcb0824b4db4a9933399c917166b3ef0e
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-05-01 10:00:38 +0000

    AbstractLoader class

---
+++ src/uc2/formats/loader.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2013 by Igor E. Novikov
+#	
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, os
+
+from uc2 import _, events, msgconst
+
+class AbstractLoader:
+	name = 'Abstract Loader'
+
+	presenter = None
+	config = None
+
+	filepath = ''
+	file = None
+	position = 0
+	file_size = 0
+
+	model = None
+
+
+	def __init__(self):pass
+
+	def load(self, presenter, path):
+		self.filepath = path
+		self.presenter = presenter
+		self.config = self.presenter.config
+		self.file_size = os.path.getsize(path)
+		try:
+			self.file = open(path, 'rb')
+		except:
+			errtype, value, traceback = sys.exc_info()
+			msg = _('Cannot open %s file for writing') % (path)
+			events.emit(events.MESSAGES, msgconst.ERROR, msg)
+			raise IOError(errtype, msg + '\n' + value, traceback)
+
+		self.do_load()
+
+		self.file.close()
+		self.position = 0
+		return self.model
+
+	def do_load(self):pass
