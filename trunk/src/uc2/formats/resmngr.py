c391d7ad04f07517739bd7ffd6a6d9adaada472d
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@f4ca8763-45e9-a9ea-a118-b73bb44545da>
2013-05-15 13:39:47 +0000

    ResourceManager implementation

--- trunk/src/uc2/formats/resmngr.py
+++ trunk/src/uc2/formats/resmngr.py
@@ -15,7 +15,20 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import os, shutil
+
+from uc2.utils import generate_id
+from uc2.utils.system import WINDOWS, get_os_family
+from uc2.formats.pdxf import const
+
+OS_FAMILY = ''
+
+def convert_resource_path(path):
+	if not OS_FAMILY:
+		OS_FAMILY = get_os_family()
+	elif OS_FAMILY == WINDOWS:
+		path = path.replace('/', '\\')
+	return path
 
 class ResourceManager:
 
@@ -23,25 +36,70 @@ class ResourceManager:
 		self.presenter = presenter
 		self.doc_dir = presenter.doc_dir
 
+	def get_resource_path(self, id):
+		ret = None
+		res_dict = self.presenter.model.resources
+		if id in res_dict.keys():
+			respath = convert_resource_path(res_dict[id])
+			path = os.path.join(self.doc_dir, respath)
+			if os.path.isfile(path):
+				ret = path
+		return ret
+
 	def get_resource(self, id):
 		ret = None
 		res_dict = self.presenter.model.resources
 		if id in res_dict.keys():
-			path = os.path.join(self.doc_dir, res_dict[id])
-			if os.path.isfile(path):
-				ret = path
+			ret = res_dict[id]
 		return ret
 
 	def copy_resources(self, rm, resources=[]):
 		for id in resources:
 			self.copy_resource(rm, id)
 
-	def copy_resource(self, rm, id):pass
+	def copy_resource(self, rm, id):
+		filepath = rm.get_resource_path(id)
+		if not filepath is None and os.path.isfile(filepath):
+			place = rm.get_resource(id).split('/')[0]
+			self.registry_file(filepath, place, id)
 
 	def delete_resources(self, resources=[], rmfile=False):
 		for id in resources:
 			self.delete_resource(id, rmfile)
 
-	def delete_resource(self, id, rmfile=False):pass
-	def registry_profile(self, filepath):pass
-	def registry_image(self, filepath):pass
+	def delete_resource(self, id, rmfile=False):
+		res_dict = self.presenter.model.resources
+		filepath = self.get_resource_path(id)
+		if id in res_dict.keys():
+			res_dict.pop(id)
+			if rmfile and not filepath is None:
+				try:
+					os.remove(filepath)
+				except:pass
+
+	def registry_file(self, filepath, place, id):
+		ret = None
+		if os.path.isfile(filepath):
+			if id is None:
+				id = generate_id()
+			filename = os.path.basename(filepath)
+			ext = os.path.splitext(filename)[1]
+			dst_filename = id + ext
+			dst_dir = os.path.join(self.doc_dir, place)
+			dst = os.path.join(dst_dir, dst_filename)
+			try:
+				shutil.copyfile(filepath, dst)
+				res_dict = self.presenter.model.resources
+				res_dict[id] = place + '/' + dst_filename
+				ret = id
+			except: pass
+		return ret
+
+	def registry_profile(self, filepath, id=None):
+		return self.registry_file(filepath, const.DOC_PROFILE_DIR, id)
+
+	def registry_image(self, filepath, id=None):
+		return self.registry_file(filepath, const.DOC_IMAGE_DIR, id)
+
+	def registry_preview(self, filepath, id=None):
+		return self.registry_file(filepath, const.DOC_PREVIEW_DIR, id)
