dadec6a8ffd1b82f0b81001d1852616b165c41c3
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-05-18 13:41:46 +0000

    refactoring

--- src/uc2/formats/pdxf/pdxf_cms.py
+++ src/uc2/formats/pdxf/pdxf_cms.py
@@ -15,21 +15,11 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from uc2 import uc2const
-
 from uc2.cms import ColorManager, CS, libcms
 
 class PDXF_ColorManager(ColorManager):
 
 	presenter = None
-	handles = {}
-	transforms = {}
-
-	use_display_profile = False
-	proofing = False
-
-	intent = uc2const.INTENT_PERCEPTUAL
-	flags = uc2const.cmsFLAGS_NOTPRECALC
 
 	def __init__(self, presenter):
 		self.presenter = presenter
@@ -37,7 +27,7 @@ class PDXF_ColorManager(ColorManager):
 
 	def update(self):
 		self.handles = {}
-		self.update_transforms()
+		self.clear_transforms()
 		profiles = self.presenter.model.profiles
 		rm = self.presenter.rm
 		if not profiles: profiles = ['', '', '', '', ]
