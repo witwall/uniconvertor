b4e968141ac8c19b79e6d9401c17ab8ffd79a0ff
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-09-01 16:07:43 +0000

    set_doc_units methods

--- src/uc2/formats/pdxf/methods.py
+++ src/uc2/formats/pdxf/methods.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2011 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -63,6 +63,9 @@ class PDXF_Methods:
 
 	def set_doc_origin(self, origin):
 		self.presenter.model.doc_origin = origin
+
+	def set_doc_units(self, units):
+		self.presenter.model.doc_units = units
 
 	def delete_object(self, obj):
 		parent = obj.parent
