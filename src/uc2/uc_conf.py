6a739a73b26f0ecb0665ded4fb7dbdaff745bf1c
igor.e.novikov <Igor.E.Novikov@gmail.com>
2013-09-13 16:49:36 +0000

    revision number update

--- src/uc2/uc_conf.py
+++ src/uc2/uc_conf.py
@@ -32,7 +32,7 @@ class UCData:
 	app_icon = None
 	doc_icon = None
 	version = '2.0'
-	revision = 'rev.340'
+	revision = 'rev.355'
 	app_config_dir = os.path.expanduser(os.path.join('~', '.config', 'uc2'))
 
 	def __init__(self):
